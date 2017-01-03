79b5a4674e1d2e3996f75ba69087d9b1323dd674
Ben Smith <bened16@users.noreply.github.com>
2017-01-03 16:38:49 +0000

    Reworked font logic and commented
    Ran into some errors when using this is my college project, simplified and corrected the messy font system.
    Added brief comments, though most code seems relatively straightforward to read.

--- TextBox.py
+++ TextBox.py
@@ -3,30 +3,32 @@ pygame.init()
 pygame.font.init()
 
 class TextBox(object):
-    def __init__(self, rect=None, text=[], bgcolour=(222,222,222), text_colour=(0,0,0), font=None):
+    def __init__(self, rect=None, text=[], bgcolour=(222,222,222), text_colour=(0,0,0), font="Arial"):
+
+#If no rectangle is specified, then a default one is chosen
         if rect is None:
             self.rect = pygame.Rect(0,0,400,25)
         else:
             self.rect = pygame.Rect(rect)
 
+#Set the options (defaults if not specified by user)
         self.text = text
         self.bgcolour = bgcolour
         self.text_colour = text_colour
-
+        self.font = pygame.font.SysFont(font, 14)
+#Set the default state to False
         self.active = False
 
-        if font is None:
-            self.font = pygame.font.Font(None, 25)
-        else:
-            self.font = font
-
     def draw(self, message, screen):
+#Draw the box
         pygame.draw.rect(screen,self.bgcolour,self.rect, 0)
+#If there is a message, render it and draw it over the box. Update the display.
         if len(message) > 0:
-            screen.blit(self.font.render("".join(message), True, self.text_colour), (self.rect.left+10, self.rect.top))
+            screen.blit(self.font.render(("".join(message)), True, self.text_colour), (self.rect.left+10, self.rect.top))
         pygame.display.flip()
 
     def text_input(self, event):
+#Grab the pressed key, do the relevant process with it
         key_input = event.key
         if key_input == pygame.K_BACKSPACE:
             if len(self.text) > 0:
@@ -38,6 +40,7 @@ class TextBox(object):
             self.text.append(chr(key_input))
 
     def test_collide(self,event):
+#Check to see if the box gets clicked, set true and  change colour
         if event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos):
             print "click!"
             self.active = True
